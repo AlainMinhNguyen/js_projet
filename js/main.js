e27f255bd41b52190bc2f8b63f504c3f3af85bd8
AlainMinhNguyen <alain.minh.nguyen@gmail.com>
2022-11-27 23:54:59 +0100

    add examples

--- js/main.js
+++ js/main.js
@@ -80,10 +80,10 @@ let myAlbum1rating = 5
 
 let myAlbum1 = new Album(myAlbum1image, myAlbum1title, myAlbum1date, myAlbum1description, myAlbum1rating);
 
-let myAlbum2image = 'https://lh3.googleusercontent.com/tLUmjnIvbPMklG1KkKE5QDuZ3DlEmhZLMGDsz5cliFgu61rYKZ93MZ_yoxEAqHTUP1DW-ICZZ2IVAac7=w544-h544-l90-rj'
-let myAlbum2title = '86 EIGHTY-SIX original soundtrack'
+let myAlbum2image = 'https://i.ytimg.com/vi/72PhV7wQr5Y/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLBiZF0kxjQPLqLYBnG-DzsdK3_Y5g'
+let myAlbum2title = 'OST | 原神/Genshin Impact/げんしん/원신'
 let myAlbum2date = new Date('2021-03-10');
-let myAlbum2description = 'Album • Hiroyuki Sawano et KOHTA YAMAMOTO • 2021 \n 42 titres • 2 heures et 33 minutes'
+let myAlbum2description = '429 vidéos'
 let myAlbum2rating = 5
 
 let myAlbum2 = new Album(myAlbum2image, myAlbum2title, myAlbum2date, myAlbum2description, myAlbum2rating);
@@ -96,18 +96,36 @@ let myGame1rating = 4
 
 let myGame1 = new Game(myGame1image, myGame1title, myGame1date, myGame1description, myGame1rating);
 
+let myGame2image = 'https://cdn1.epicgames.com/salesEvent/salesEvent/EGS_GenshinImpact_miHoYoLimited_S2_1200x1600-c12cdcc2cac330df2185aa58c508e820'
+let myGame2title = 'Genshin Impact'
+let myGame2date = new Date('2021-03-10');
+let myGame2description = 'Black Desert Online is a sandbox-oriented fantasy massively multiplayer online role-playing game developed by Korean video game developer Pearl Abyss and originally published for Microsoft Windows in 2015.'
+let myGame2rating = 4
+
+let myGame2 = new Game(myGame2image, myGame2title, myGame2date, myGame2description, myGame2rating);
+
 let myMovie1image = 'https://www.nautiljon.com/images/anime/00/20/mini/fate_stay_night_heaven_s_feel_i_presage_flower_4702.jpg?11528565156'
 let myMovie1title = 'Fate/stay night: Heaven\'s Feel I. presage flower'
 let myMovie1date = new Date('2021-03-10');
-let myMovie1description = 'Fate/stay night: Heaven\'s Feel is a Japanese anime film trilogy produced by Ufotable, directed by Tomonori Sudō, written by Akira Hiyama, and featuring music by Yuki Kajiura. The trilogy adapts Heaven\'s Feel, the third and final route of the Fate/stay night visual novel.'
-let myMovie1rating = 3
+let myMovie1description = 'Genshin Impact is an action role-playing game developed and published by miHoYo. It was released for Microsoft Windows, PlayStation 4, iOS, and Android in 2020, on PlayStation 5 in 2021, and is set for release on Nintendo Switch.'
+let myMovie1rating = 4
 
 let myMovie1 = new Movie(myMovie1image, myMovie1title, myMovie1date, myMovie1description, myMovie1rating);
+
+let myMovie2image = 'https://www.nautiljon.com/images/anime/00/91/mini/fate_stay_night_heaven_s_feel_ii_lost_butterfly_5719.jpg?11567168184'
+let myMovie2title = 'Fate/stay night: Heaven\'s Feel II. lost butterfly'
+let myMovie2date = new Date('2021-03-10');
+let myMovie2description = 'Fate/stay night: Heaven\'s Feel II. lost butterfly is a 2019 Japanese anime fantasy film produced by ufotable and directed by Tomonori Sudō. The second installment in the Fate/stay night: Heaven\'s Feel trilogy, it premiered in Japan on January 12, 2019 and in the United States on March 14, 2019.'
+let myMovie2rating = 3
+
+let myMovie2 = new Movie(myMovie2image, myMovie2title, myMovie2date, myMovie2description, myMovie2rating);
 
 myCollection.add(myAlbum1);
 myCollection.add(myGame1);
 myCollection.add(myMovie1);
 myCollection.add(myAlbum2);
+myCollection.add(myMovie2);
+myCollection.add(myGame2);
 
 myCollection.items.forEach(element => {
     console.log(element);
