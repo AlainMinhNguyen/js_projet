ae6b9ec1fa3c78b5b04db8a7a69393adc9ec7ea8
AlainMinhNguyen <alain.minh.nguyen@gmail.com>
2022-11-27 18:29:31 +0100

    init classes

---
+++ js/Media.js
@@ -0,0 +1,8 @@
+export class Media {
+    constructor(image, title, description, rating) {
+        this.image = image;
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+    }
+}
